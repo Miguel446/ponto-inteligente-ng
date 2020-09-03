71f6ecb5f117fc0255bf3fb539db84a335ef4728
Itallo <italloazevedo@hotmail.com>
2020-09-03 15:19:05 -0300

    cadastro pj concluído

--- src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
+++ src/app/autenticacao/cadastro-pj/cadastro-pj.module.ts
@@ -2,8 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { CadastrarPjComponent, CadastroPjComponent } from './components';
+import { CadastroPjService } from './services';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -13,7 +24,19 @@ import { CadastrarPjComponent, CadastroPjComponent } from './components';
   imports: [
     CommonModule,
     RouterModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatInputModule,
+    MatButtonModule,
+    MatListModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSnackBarModule,
+    SharedModule
+  ],
+  providers: [
+    CadastroPjService
   ]
 })
 export class CadastroPjModule { }
